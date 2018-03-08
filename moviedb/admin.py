b92c87c7997be2f2caf817ef252bd782bf3feb95
mwerumuchai <mwerumuchai@gmail.com>
2018-03-08 04:21:28 +0300

    create models

--- moviedb/admin.py
+++ moviedb/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
-from .models import Actors
+from .models import Actors,Directors,Movie
 
 admin.site.register(Actors)
+admin.site.register(Directors)
+admin.site.register(Movie)
--- moviedb/models.py
+++ moviedb/models.py
@@ -16,3 +16,30 @@ class Actors(models.Model):
 
     def __str__(self):
         return self.actor_name
+
+class Directors(models.Model):
+    director_name = models.CharField(max_length=150,blank=False)
+
+
+    def save_actor(self):
+        self.save()
+
+    @classmethod
+    def get_category(cls):
+        director = Directors.objects.all()
+
+        return director
+
+    def __str__(self):
+        return self.director_name
+
+class Movie(models.Model):
+    movie_title = models.TextField(max_length=600)
+    movie_year = models.IntegerField(blank=False)
+    movie_length = models.IntegerField(blank=False)
+    country = models.TextField(max_length=600)
+    actor = models.ForeignKey(Actors,on_delete=models.CASCADE)
+    director = models.ForeignKey(Directors,on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.movie_title
